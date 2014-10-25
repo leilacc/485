6af0ecd1aab293107d0ada3e8165f7489b7f9793
Kasra Kyanzadeh <me@kasrak.com>
2014-10-25 18:18:21 -0400

    q2a

--- a3/q2a.py
+++ a3/q2a.py
@@ -1,29 +1,21 @@
 preps = ['by', 'onto', 'toward', 'beside', 'for', 'among']
 verbs = ['swam', 'hurried', 'placed', 'advanced','love', 'admire']
-nouns = ['seals' ,'otters', 'we']
-
-
+nouns = ['fish', 'whales', 'seals' ,'otters', 'we']
 
 unambiguous = []
-potential_tuple = []
-verb_state = 0
-noun_state = 0
+count = {}
 with open('pp-corpus', 'r') as corpus:
     for line in corpus:
         words = line.split()
-        for word in words:
-            if word in verbs and not verb_state:
-                potential_tuple = [word]
-                verb_state = 1
-                noun_state = 0
-            elif word in nouns and not noun_state:
-                potential_tuple = [word]
-                verb_state = 0
-                noun_state = 1
-            elif word in preps and (noun_state or verb_state):
-                potential_tuple.append(word)
-                unambiguous.append(potential_tuple)
-                verb_state = 0
-                noun_state = 0
+        for i, word in enumerate(words):
+            if (word in verbs or word in nouns) and (
+                i + 1 < len(words) and words[i + 1] in preps):
+                pair = (word, words[i + 1])
+                if pair not in count:
+                    count[pair] = 1
+                    unambiguous.append(pair)
+                else:
+                    count[pair] += 1
 
-print unambiguous
+for pair in unambiguous:
+    print "%d %s %s" % (count[tuple(pair)], pair[0], pair[1])
