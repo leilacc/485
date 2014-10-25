0568af958d5d3b066ceb79a79006b0d6276aa01b
Kasra Kyanzadeh <me@kasrak.com>
2014-10-25 18:55:27 -0400

    q2a

--- a3/q2a.py
+++ a3/q2a.py
@@ -1,3 +1,11 @@
+def add_unambig(word1, pp, count, unambiguous):
+    pair = (word1, pp)
+    if pair not in count:
+        count[pair] = 1
+        unambiguous.append(pair)
+    else:
+        count[pair] += 1
+
 preps = ['by', 'onto', 'toward', 'beside', 'for', 'among']
 verbs = ['swam', 'hurried', 'placed', 'advanced','love', 'admire']
 nouns = ['fish', 'whales', 'seals' ,'otters', 'we']
@@ -8,14 +16,22 @@ with open('pp-corpus', 'r') as corpus:
     for line in corpus:
         words = line.split()
         for i, word in enumerate(words):
-            if (word in verbs or word in nouns) and (
-                i + 1 < len(words) and words[i + 1] in preps):
-                pair = (word, words[i + 1])
-                if pair not in count:
-                    count[pair] = 1
-                    unambiguous.append(pair)
-                else:
-                    count[pair] += 1
+            if word in preps:
+                for j in [1, 2]:
+                    new_index = i - j
+                    if new_index < 0:
+                        break
+                    if words[new_index] in verbs and words[i - 1] not in nouns:
+                        add_unambig(words[new_index], word, count, unambiguous)
+                        print "%s %s %s" % (words[new_index], words[i - 1], word)
+                        break
+                    elif words[new_index] in nouns and words[i - 2] not in verbs:
+                        add_unambig(words[new_index], word, count, unambiguous)
+                        print "%s %s %s" % (words[new_index], words[i - 1], word)
+                        break
+
+
+
 
 for pair in unambiguous:
     print "%d %s %s" % (count[tuple(pair)], pair[0], pair[1])
